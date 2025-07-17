2956a09094a16412c8fb1794f704b01e82a6725e
Hasibur Rahman <113811769+inbox-hasibur@users.noreply.github.com>
2025-07-17 17:11:48 +0600

    commit

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,29 @@
-
 import React from 'react';
 import './Navbar.css';
 import logo from '../../assets/Event Management Logo.png';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-          <div className="navbar__logo">
+    <header className="header">
+      <div className="navbar-container">
+        <div className="navbar__main">
+          <div className="navbar__brand">
             <img src={logo} alt="Event Management Logo" className="navbar__logo-img" />
           </div>
-      <ul className="navbar__links">
-        <li className="active">Home</li>
-        <li>Events</li>
-        <li>About Us</li>
-        <li>Contact</li>
-      </ul>
-      <div className="navbar__actions">
-        <button className="login-btn">Login</button>
-        <button className="register-btn">Register</button>
+          <ul className="navbar__links">
+            <li className="active">Home</li>
+            <li>Events</li>
+            <li>About Us</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="navbar__auth">
+          <button className="login-btn">Login</button>
+          <button className="register-btn">Register</button>
+        </div>
       </div>
-    </nav>
-  )
+    </header>
+  );
 };
 
 export default Navbar;
